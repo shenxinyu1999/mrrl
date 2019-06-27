bb8c7bcd3941adf92036885653161b98b7a724ed
Kevin Doyon <kevin.doyon@gmail.com>
2019-06-27 12:20:03 -0400

    Updates

---
+++ precache-manifest.053722d66d72df18fb8ae73ee530baf8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a32139de8e09f0d9fdd3c0557f435d5f",
+    "url": "/mrrl/index.html"
+  },
+  {
+    "revision": "50d33e0f52a11cc458b4",
+    "url": "/mrrl/static/css/main.d2b3647d.chunk.css"
+  },
+  {
+    "revision": "0c977abaccb90f660f24",
+    "url": "/mrrl/static/js/2.ded8ad7d.chunk.js"
+  },
+  {
+    "revision": "50d33e0f52a11cc458b4",
+    "url": "/mrrl/static/js/main.8627960f.chunk.js"
+  },
+  {
+    "revision": "d9cb38978b828b655a46",
+    "url": "/mrrl/static/js/runtime~main.d17881e3.js"
+  }
+]);
